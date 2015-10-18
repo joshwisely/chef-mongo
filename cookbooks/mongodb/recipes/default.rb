e29ecfdaf304b82852682586557d0e019857561d
Josh Wisely <joshwisely@gmail.com>
2015-10-18 04:06:01 -0700

    Update default recipe to include repo info.

--- cookbooks/mongodb/recipes/default.rb
+++ cookbooks/mongodb/recipes/default.rb
@@ -1,15 +1,23 @@
+#Add the MongoDB repository
+yum_repository 'mongodo' do
+  description "MongoDB Repository"
+  baseurl "https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.0/x86_64/"
+  gpgcheck :false
+  action :create
+end
+
 #MongoDB daemon and associated configuration and init scripts.
-package "mongodb-org-server"
+package "mongodb-org-server" do
   action :install
 
 #MondoDB mongos daemon.
-package "mongodb-org-mongos"
+package "mongodb-org-mongos" do
   action :install
 
 #MongoDB shell.
-package "mongodb-org-shell"
+package "mongodb-org-shell" do
   action :install
 
 #MongoDB tools.
-package "mongodb-org-tools"
+package "mongodb-org-tools" do
   action :install
