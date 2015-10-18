799bf27a2a55f1fb1a5850ac5bb26abb232a19ec
Josh Wisely <joshwisely@gmail.com>
2015-10-18 03:33:19 -0700

    Adding basic files from starter kit.

---
+++ .chef/knife.rb
@@ -0,0 +1,11 @@
+# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "admin"
+client_key               "#{current_dir}/admin.pem"
+validation_client_name   "test-validator"
+validation_key           "#{current_dir}/test-validator.pem"
+chef_server_url          "https://chef-server-02.btsys.org/organizations/test"
+cookbook_path            ["#{current_dir}/../cookbooks"]
