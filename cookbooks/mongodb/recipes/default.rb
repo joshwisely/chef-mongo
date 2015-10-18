24f1edfe71e45c000bba26be3b5d08724db69e3f
Josh Wisely <joshwisely@gmail.com>
2015-10-18 04:14:28 -0700

    Updated recipe to properly set gpgcheck to false.

--- cookbooks/mongodb/recipes/default.rb
+++ cookbooks/mongodb/recipes/default.rb
@@ -1,23 +1,27 @@
 #Add the MongoDB repository
 yum_repository 'mongodo' do
-  description "MongoDB Repository"
-  baseurl "https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.0/x86_64/"
-  gpgcheck :false
+  description 'MongoDB Repository'
+  baseurl 'https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.0/x86_64/'
+  gpgcheck FALSE
   action :create
 end
 
 #MongoDB daemon and associated configuration and init scripts.
-package "mongodb-org-server" do
+package 'mongodb-org-server' do
   action :install
+end
 
 #MondoDB mongos daemon.
-package "mongodb-org-mongos" do
+package 'mongodb-org-mongos' do
   action :install
+end
 
 #MongoDB shell.
-package "mongodb-org-shell" do
+package 'mongodb-org-shell' do
   action :install
+end
 
 #MongoDB tools.
-package "mongodb-org-tools" do
+package 'mongodb-org-tools' do
   action :install
+end
