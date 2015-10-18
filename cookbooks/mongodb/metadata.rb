344e4cf92b1f2f74ea237ced0377a82ffc5bb6f6
Josh Wisely <joshwisely@gmail.com>
2015-10-18 07:04:53 -0700

    Add firewalld. Update test for mongo daemon.

--- cookbooks/mongodb/metadata.rb
+++ cookbooks/mongodb/metadata.rb
@@ -1,8 +1,13 @@
 name               'mongodb'
-version            '0.0.10'
+version            '0.1.1'
 maintainer         'Josh Wisely'
 maintainer_email   'joshwisely@gmail.com'
-supports           'centos'
+description        'Installs/Configures MongoDB'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+
+supports           'centos', ">= 7.0"
+supports           'rhel', ">= 7.0"
+
 depends            'yum', '~> 3.8.1'
 depends            'selinux', '~> 0.9.0'
 depends            'firewalld', '~> 1.1.1'
