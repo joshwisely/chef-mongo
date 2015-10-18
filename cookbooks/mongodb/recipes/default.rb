d169734e89954e8075a1680f8679a0ae3506ccca
Josh Wisely <joshwisely@gmail.com>
2015-10-18 04:40:43 -0700

    Fixed version for MongoDB in recipe to ensure it doesn't change unless I want it to.

--- cookbooks/mongodb/recipes/default.rb
+++ cookbooks/mongodb/recipes/default.rb
@@ -9,19 +9,23 @@ end
 #MongoDB daemon and associated configuration and init scripts.
 package 'mongodb-org-server' do
   action :install
+  version '3.0.7-1.el7'
 end
 
 #MondoDB mongos daemon.
 package 'mongodb-org-mongos' do
   action :install
+  version '3.0.7-1.el7'
 end
 
 #MongoDB shell.
 package 'mongodb-org-shell' do
   action :install
+  version '3.0.7-1.el7'
 end
 
 #MongoDB tools.
 package 'mongodb-org-tools' do
   action :install
+  version '3.0.7-1.el7'
 end
